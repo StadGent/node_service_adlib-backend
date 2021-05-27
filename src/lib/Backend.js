11782d74e8db331d88c1576610b5583c76f653a9
Unknown <achrafatauil@hotmail.com>
2021-05-27 12:55:14 +0200

    remove unused variables and imports

--- src/lib/Backend.js
+++ src/lib/Backend.js
@@ -1,11 +1,9 @@
 import { Writable } from 'stream';
 import Utils from './utils.js';
-import Config from "../config/config.js";
 
 class Backend extends Writable {
     constructor(options) {
         super();
-        const config = Config.getConfig();
 
         this._adlibDatabase = options.adlibDatabase;
         this._institution = options.institution;
