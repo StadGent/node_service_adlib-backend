502c43b92e77e981f103579b109542a5887f5954
brechtvdv <br5cht@hotmail.com>
2021-09-14 10:40:12 +0200

    Add dcterms:isVersionOf @id to context of termen

--- src/lib/mappers/termenMapper.js
+++ src/lib/mappers/termenMapper.js
@@ -14,7 +14,10 @@ export default class TermenMapper extends Transform {
             {
                "skos": "http://www.w3.org/2004/02/skos/core#",
                 "owl": "http://www.w3.org/2002/07/owl#",
-                "opmerking": "http://www.w3.org/2004/02/skos/core#note"
+                "opmerking": "http://www.w3.org/2004/02/skos/core#note",
+                "dcterms:isVersionOf": {
+                    "@type": "@id"
+                }
             }
         ];
         this._adlibDatabase = options.adlibDatabase;
