684242801ea11ca1f7de0e4fdc1163bcaf806b65
brechtvdv <br5cht@hotmail.com>
2021-11-24 15:23:45 +0100

    Fix endless loop in termenMapper

--- src/lib/mappers/termenMapper.js
+++ src/lib/mappers/termenMapper.js
@@ -392,6 +392,8 @@ export default class TermenMapper extends Transform {
                     }
                 }
                 done(null, JSON.stringify(mappedObject));
+            } else {
+                done();
             }
         } catch (e) {
             console.error(e);
