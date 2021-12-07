4768e3aeaf12452edfdd285953ec2478dfc01d1a
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2021-12-07 15:38:44 +0100

    update mapIIIFManifest in mappers

--- src/lib/mappers/hvaMapper.js
+++ src/lib/mappers/hvaMapper.js
@@ -50,7 +50,7 @@ export default class HvAMapper extends ObjectMapper {
             await Utils.mapIconografie(input, mappedObject, this._adlib);
 
             // reproductie
-            await Utils.mapIIIFManifest(input, mappedObject);
+            await Utils.mapIIIFManifest(input, mappedObject, MainUtils);
 
 
             done(null, JSON.stringify(mappedObject));
