37f22631dbb6f88067b56f97bb3e1fa463e499ee
Olivier Van D'huynslager <43210443+oliviervd@users.noreply.github.com>
2023-06-19 10:17:35 +0200

    add virtual split for db (dmg) --> objecten + archief.

--- src/lib/Backend.js
+++ src/lib/Backend.js
@@ -5,6 +5,7 @@ class Backend extends Writable {
     constructor(options) {
         super();
 
+        this._id = options.id;
         this._adlibDatabase = options.adlibDatabase;
         this._institution = options.institution;
         this._db = options.db;
@@ -14,8 +15,14 @@ class Backend extends Writable {
 
 Backend.prototype._write = function (chunk, encoding, done) {
     // write object to file
+    let db = this._adlibDatabase;
+
+    // hack to split virtual db (is merged in one but are seperate dbs.).
+    if (this._id === "dmg-archief") {
+        db = "archief"
+    }
     let object = JSON.parse(chunk);
-    Utils.insertObject(this._institution, this._db, object, this._adlibDatabase, this._correlator);
+    Utils.insertObject(this._institution, this._db, object, db, this._correlator);
     done();
 };
 
